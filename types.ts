acf27ef4c3afed05d77535cec8379dd438a7e214
Ntale3 <swamaduntale98@gmail.com>
2025-08-04 16:42:57 +0300

    ♻️  add types for destinationation imapage

--- types.ts
+++ types.ts
@@ -21,3 +21,27 @@ export interface FilterOption {
   value: string;
   label: string;
 }
+
+export interface DestinationType{
+id: number,
+name: string,
+location: string,
+category: string,
+image: string,
+price: string,
+rating: number,
+reviews: number,
+duration: string,
+groupSize: string,
+description: string,
+highlights: string[],
+bestTime: string
+}
+
+export interface categoryType
+{
+ id:string,  
+ name:string,
+ icon?:string
+
+}
