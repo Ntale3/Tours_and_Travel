93de02e6f011fdaf804921e78e7facf1aa71693f
Ntale Samad <swamaduntale98@gmail.com>
2025-12-13 07:34:50 +0300

    ✨ nav-main compinets

---
+++ components/nav-main.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import { IconCirclePlusFilled, IconMail, type Icon } from "@tabler/icons-react"
+import { $navigationStore, setActivePage } from "@/store/navigation.store";
+import { observer } from "@legendapp/state/react";
+
+import { Button } from "@/components/ui/button"
+import {
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+export const NavMain = observer(({
+ items,
+}: {
+items: {
+title: string
+url: string
+icon?: Icon
+}[]
+}) => {
+    const activePage = $navigationStore.activePage.get();
+    return (
+        <SidebarGroup>
+            <SidebarGroupContent className="flex flex-col gap-2">
+                <SidebarMenu>
+                    <SidebarMenuItem className="flex items-center gap-2">
+                        <SidebarMenuButton
+                            tooltip="Quick Create"
+                            className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+                        >
+                            <IconCirclePlusFilled />
+                            <span>Quick Create</span>
+                        </SidebarMenuButton>
+                        <Button
+                            size="icon"
+                            className="size-8 group-data-[collapsible=icon]:opacity-0"
+                            variant="outline"
+                        >
+                            <IconMail />
+                            <span className="sr-only">Inbox</span>
+                        </Button>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+                <SidebarMenu>
+                    {items.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton
+                                tooltip={item.title}
+                                onClick={() => setActivePage(item.title)}
+                                isActive={activePage === item.title}
+                            >
+                                {item.icon && <item.icon />}
+                                <span>{item.title}</span>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    )
+});
